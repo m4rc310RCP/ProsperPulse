1b5b65911f69217f5ef35c41ccc42f9a038c300e
m4rc310RCP <marcelo.utfpr@me.com>
2024-02-17 15:38:40 -0300

    send new version

--- src/main/java/br/com/m4rc310/configurations/GraphQLConfig.java
+++ src/main/java/br/com/m4rc310/configurations/GraphQLConfig.java
@@ -4,26 +4,36 @@ import org.springframework.context.annotation.Configuration;
 
 import br.com.m4rc310.gql.MUserProvider;
 import br.com.m4rc310.gql.dto.MUser;
+import lombok.extern.slf4j.Slf4j;
 
+@Slf4j
 @Configuration
 public class GraphQLConfig extends MUserProvider {
-
+	
 	@Override
 	public MUser authUser(String username, Object password) throws Exception {
-		// TODO Auto-generated method stub
-		return null;
+		
+		log.info("AuthUser {}", username);
+		
+		MUser user = new MUser();
+		user.setUsername(username);
+		user.setPassword(String.valueOf(password));
+		user.setRoles("admin".split(";"));
+		return user;
 	}
 
 	@Override
 	public MUser getUserFromUsername(String username) {
-		// TODO Auto-generated method stub
-		return null;
+		log.info("getUserFromUsername {}", username);
+		MUser user = new MUser();
+		user.setUsername(username);
+		return user;
 	}
 
 	@Override
 	public boolean isValidUser(MUser user) {
-		// TODO Auto-generated method stub
-		return false;
+		log.info("isValidUser {}", user);
+		return true;
 	}
 
 }
--- src/main/java/br/com/m4rc310/services/MService.java
+++ src/main/java/br/com/m4rc310/services/MService.java
@@ -2,6 +2,7 @@ package br.com.m4rc310.services;
 
 import br.com.m4rc310.services.consts.Const;
 
-public class MService extends br.com.m4rc310.gql.services.MService implements Const{
+public class MService extends br.com.m4rc310.gql.services.MService  implements Const {
 
 }
+
